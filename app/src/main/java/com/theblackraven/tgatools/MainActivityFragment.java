c1ab3c489c5d32caff06b1d845c1f322b5425e64
theblackraven <theblackraven@web.de>
2016-06-04 11:47:06 +0200

    Kapitel 4 abgeschlossen http://www.programmierenlernenhq.de/tutorial-android-listview-verwenden/http://www.programmierenlernenhq.de/tutorial-android-listview-verwenden/

--- app/src/main/java/com/theblackraven/tgatools/MainActivityFragment.java
+++ app/src/main/java/com/theblackraven/tgatools/MainActivityFragment.java
@@ -8,6 +8,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import android.widget.ListView;
 
 public class MainActivityFragment extends Fragment{
 
@@ -17,8 +22,26 @@ public class MainActivityFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String [] ToolsListeArray = {
+                "Wärmeleistung",
+                "Druckverluste"
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        }; //array mit Daten
+
+        List <String> ToolsListe = new ArrayList<>(Arrays.asList(ToolsListeArray));
+
+        ArrayAdapter <String> ToolsListAdapter =
+                new ArrayAdapter<>(
+                        getActivity(), // Die aktuelle Umgebung (diese Activity)
+                        R.layout.list_item_tools, // ID der XML-Layout Datei
+                        R.id.list_item_tools_textview, // ID des TextViews
+                        ToolsListe); // Beispieldaten in einer ArrayList
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView ToolsListView = (ListView) rootView.findViewById(R.id.listview_aktienliste);
+        ToolsListView.setAdapter(ToolsListAdapter);
+
+        return rootView;
 
     }
 
