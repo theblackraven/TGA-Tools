7f19f7777ec1daa2eb67197b19423147dbaa3993
theblackraven <theblackraven@web.de>
2016-06-04 11:14:18 +0200

    Kapitel 4? vom http://www.programmierenlernenhq.de/tutorial-android-listview-verwenden/http://www.programmierenlernenhq.de/tutorial-android-listview-verwenden/

---
+++ app/src/androidTest/java/com/theblackraven/tgatools/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.theblackraven.tgatools;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/theblackraven/tgatools/MainActivityFragment.java
@@ -0,0 +1,26 @@
+package com.theblackraven.tgatools;
+
+/**
+ * Created by x220 on 29.05.2016.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityFragment extends Fragment{
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_main, container, false);
+
+    }
+
+}
+
